7789bcd8f4ee71b74eeb36057ac957dfde0e3893
nsivasiva <96908332+nsivasiva@users.noreply.github.com>
2022-01-08 13:52:17 +0530

    Add files via upload

---
+++ WEB/__game-scripts.js
@@ -0,0 +1,1 @@
+var WebarSdkSurface=pc.createScript("WebarSdkSurface");WebarSdkSurface.attributes.add("webarSdkCamera",{title:"Webar Camera",type:"entity"}),WebarSdkSurface.attributes.add("webarSdkStage",{title:"Webar Stage",type:"entity"}),WebarSdkSurface.prototype.initialize=function(){WEBARSDK.InitPlaycanvas("application-canvas",this.webarSdkCamera,this.webarSdkStage)};
---
+++ WEB/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1307441.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 64834156 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
